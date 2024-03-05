07d0521390443ab75dffab7e0a9c18f5abb7d169
Fizzchimp <callumaskew@gmail.com>
2024-03-05 22:25:36 +0000

    Test Pygame Window

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pygame
+
 pygame.init()
+screen = pygame.display.set_mode([500, 500])
+screen.fill((255, 255, 255))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    pygame.display.flip()
+
+pygame.quit()
