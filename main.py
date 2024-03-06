be3ed19349aff25dd3f90d1de8572da73212ae49
Callum Askew <18askewc@staidans.co.uk>
2024-03-06 13:28:15 +0000

    fejioxaPUJHCVFED

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ class World:
         pygame.init()
         self.screen = pygame.display.set_mode([HEIGHT, WIDTH])
         self.running = True
-        self.x = self.y = 125
+        self.x = 125
+        self.y = 300
         self.box = pygame.Rect(self.x, self.y, 100, 100)
-        
+        self.momentumX = self.momentumY = 1
     
     def scrUpdate(self):
-        self.x += 1
-        self.y += 1
+        self.x += self.momentumX
+        self.y += self.momentumY
+        print(self.x)
         self.box.update(self.x, self.y, 100, 100)
         self.screen.fill((100, 100, 100))
-        pygame.draw.rect(self.screen, (125, 0, 75), self.box)
+        if self.collision():
+            pygame.quit()
+        pygame.draw.rect(self.screen, (100, 0, 75), self.box)
         pygame.display.flip()
+       
+    def collision(self):
+        if self.x == 400 or self.x == 0:
+            self.momentumX *= -1
+        if self.y == 400 or self.y == 0:
+            self.momentumY *= -1
 
 
 world = World(500, 500)
