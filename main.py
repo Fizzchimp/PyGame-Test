524d9e853891a1c1bbcf8f70b0f3818fd9f5a2f7
18askewc <18askewc@staidans.co.uk>
2024-03-06 12:21:01 +0000

    Pygame Testing

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode([500, 500])
-screen.fill((255, 255, 255))
+class World:
+    def __init__(self, HEIGHT, WIDTH):
+        pygame.init()
+        self.screen = pygame.display.set_mode([HEIGHT, WIDTH])
+        self.running = True
+        self.x = self.y = 125
+        self.box = pygame.Rect(self.x, self.y, 100, 100)
+        
+    
+    def scrUpdate(self):
+        self.x += 1
+        self.y += 1
+        self.box.update(self.x, self.y, 100, 100)
+        self.screen.fill((100, 100, 100))
+        pygame.draw.rect(self.screen, (125, 0, 75), self.box)
+        pygame.display.flip()
 
-running = True
-while running:
+
+world = World(500, 500)
+while world.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    pygame.display.flip()
+            world.running = False
+ 
+    world.scrUpdate()
+    
+    pygame.time.wait(10)
 
 pygame.quit()
