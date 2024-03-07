73f11eaa4e5c7ca11a6b9c71520a7d509894a824
Callum Askew <18askewc@staidans.co.uk>
2024-03-07 11:17:26 +0000

    aTTEMPT

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ class World:
         self.running = True
 
         self.screenDims = (WIDTH, HEIGHT)
-        self.pos = [WIDTH / 2, HEIGHT / 2]
-        self.momtm = [1, 0.5]
+        self.point = self.pos = [WIDTH / 2, HEIGHT / 2]
+        self.momtm = [1, 0.25]
+        self.lines = []
 
         # self.box = pygame.Rect(self.x, self.y, 100, 100)
         self.image = pygame.transform.scale(pygame.image.load("krish.png").convert(), (100, 100))
@@ -21,6 +22,9 @@ class World:
         self.screen.fill((100, 100, 100))
         self.pos[0] += self.momtm[0]
         self.pos[1] += self.momtm[1]
+        for line in self.lines:
+            print("A")
+            pygame.draw.line(self.screen, (255, 0, 0), line[0], line[1])
         self.screen.blit(self.image, (self.pos[0], self.pos[1]))
         self.collision()
 
@@ -32,11 +36,17 @@ class World:
     def collision(self):
         if self.pos[0] == self.screenDims[0] - 100 or self.pos[0] == 0:
             self.momtm[0] *= -1
+            self.lineDraw(self.pos)
         if self.pos[1] == self.screenDims[1] - 100 or self.pos[1] == 0:
             self.momtm[1] *= -1
+            self.lineDraw(self.pos)
+            
+    def lineDraw(self, point):
+        self.lines.append([self.point, point])
+        self.point = point
 
 
-world = World(750, 500)
+world = World(948, 502)
 while world.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,6 +54,6 @@ while world.running:
  
     world.scrUpdate()
     
-    pygame.time.wait(10)
+    pygame.time.wait(4)
 
 pygame.quit()
