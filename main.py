17d16c352231eb66105e389034f010abdc36084b
Fizzchimp <callumaskew@gmail.com>
2024-03-10 18:21:56 +0000

    Physics finished

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ class World:
         self.screenDims = (WIDTH, HEIGHT)
         self.point = self.pos = [WIDTH / 2, HEIGHT / 2]
         self.lines = []
-        self.boxes = [Box([200,200], [500, 100], [1, 1], "03_sopie.png"),
-                      Box([200, 200], [710, 100], [1, 1], "01_krish.png")]
-                    #   Box([100, 100], [500, 100], [1, -0.25], "04_hana.png"), 
-                    #   Box([100, 100], [500, 300], [1, -0.25], "05_ella.png"),
-                    #   Box([100, 100], [300, 300], [1, -0.25], "06_rob.png")]
+        self.boxes = [Box([100, 100], [700, 50], [1, -0.5], "03_sopie.png"),
+                      Box([100, 100], [700, 254], [-1, 1], "01_krish.png"),
+                      Box([100, 100], [500, 100], [-0.25, 0.25], "04_hana.png"), 
+                      Box([100, 100], [500, 300], [1, 0.5], "05_ella.png"),
+                      Box([100, 100], [300, 300], [-1, -0.25], "06_rob.png")]
         pygame.display.set_icon(self.boxes[0].image)
         
     
@@ -25,23 +25,33 @@ class World:
         
             
         for i, box in enumerate(self.boxes):
+            self.colDetect(box, i)
+
+        for box in self.boxes:
             box.pos[0] += box.momtm[0]
             box.pos[1] += box.momtm[1]
             self.screen.blit(box.image, (box.pos[0], box.pos[1]))
             box.move(box.pos)
-            self.colDetect(box)
         
         pygame.display.flip()
        
-    def colDetect(self, object):
-        for box2 in self.boxes:
-            if object.colRect.colliderect(box2.colRect) and box2 != object:
-                print(object.pos, box2.pos)
+    def colDetect(self, object,  i):
+        for j, box2 in enumerate(self.boxes):
+            if object.colRect.colliderect(box2.colRect) and i != j:
 
-                if object.pos[0] >= box2.pos[0] + 200 or object.pos[0] <= box2.pos[0] - 200:
-                    object.momtm[0] *= -1
-                else:
-                    object.momtm[1]  *= -1
+                if 1 <= object.colRect.right - box2.colRect.left <= 2:
+                    object.momtm[0] = -abs(object.momtm[0])
+
+                elif 1 <= box2.colRect.right - object.colRect.left <= 2:
+                    object.momtm[0] = abs(object.momtm[0])
+
+
+                if 1 <= object.colRect.bottom - box2.colRect.top <= 2:
+                    object.momtm[1] = -abs(object.momtm[1])
+
+                elif 1 <= box2.colRect.bottom - object.colRect.top <= 2:
+                    object.momtm[1] = abs(object.momtm[1])
+
                     
 
         if object.pos[0] == self.screenDims[0] - object.dims[0] or object.pos[0] == 0:
@@ -65,6 +75,6 @@ while world.running:
             world.running = False
  
     world.scrUpdate()
-    pygame.time.wait(40)
+    pygame.time.wait(4)
 
 pygame.quit()
